bbaa82a72620fa43be192958f2a08df5d5f8050f
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-27 23:34:49 +0530

    feat: Added: Multiple AddressBook With Multiple PersonDetails

--- NewAddressBook/Program.cs
+++ NewAddressBook/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
-namespace NewAddressBook
+namespace AddressBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Welcome Message
-            Console.WriteLine("Welcome TO AddressBook!");
-            Console.WriteLine("=======================");
+            //Display Welcome Message
+            Console.WriteLine("Welcome to Address Book Management System Development!");
+            Console.WriteLine("======================================================");
+
+            //Call AddressBook Options
+            AddressBookBuilder addressBookBuilder = new AddressBookBuilder();
+            addressBookBuilder.AddressBookCreator();
+
+            //addressBookBuilder.Display(); //Display Contact
+            //addressBookBuilder.ModifyContact();//Modify Contact
+            //addressBookBuilder.DeleteContact();//Delete Contact
         }
     }
 }
