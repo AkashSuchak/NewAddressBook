ffb6a3c7a83b9a713f1980cccecaea4ca87a3118
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-27 18:20:23 +0530

    Add project files.

---
+++ NewAddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NewAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome TO AddressBook!");
+            Console.WriteLine("=======================");
+        }
+    }
+}
